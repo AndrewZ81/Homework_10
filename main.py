c507bb0e044fc4408d69ff8420d3c919100b2e93
Andrew Zaiko <andrewzaiko@gmail.com>
2022-07-09 20:22:30 +0300

    Добавил новые маршруты

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def page_index():
     return "ПРИВЕТ!"
 
+@app.route("/profile/")
+def page_profile():
+    return "Профиль пользователя"
+
+@app.route("/feed/")
+def page_feed():
+    return "Лента пользователя"
+
+@app.route("/messages/")
+def page_messages():
+    return "Сообщения пользователя"
+
 if __name__ == "__main__":
     app.run(host="127.0.0.2", port=80)
